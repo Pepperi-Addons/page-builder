911d625af0d9b44945734e0c5668ed29f5e99ce9
avnere_pepperi <avner.e@pepperi.com>
2021-08-01 10:12:11 +0300

    no message

--- sub-addons/sub-addon-1/client-side/src/app/components/addon/pepperi-table.component.ts
+++ sub-addons/sub-addon-1/client-side/src/app/components/addon/pepperi-table.component.ts
@@ -10,6 +10,7 @@ import { PepListComponent } from '@pepperi-addons/ngx-lib/list';
 
       [firstFieldAsLink]="false"
       [isReport]="true"
+      [viewType]="'table'"
       [supportSorting]="false"
       [supportResizing]="false"
       [selectionTypeForActions]="'single'"
@@ -59,7 +60,7 @@ export class PepperiTableComponent implements OnInit, OnChanges {
             dataSource.forEach((row: any) => tableData.push(this.convertObjectToPepRowData(row, displayedColumns, customizeFn)));
             const uiControl = this.dataConvertorService.getUiControl(tableData[0]);
             const rows = this.dataConvertorService.convertListData(tableData);
-            this.customList.initListData(uiControl, rows.length, rows, 'table', '', true);
+            this.customList.initListData(uiControl, rows.length, rows);
         }
     }
 
