dc839beafa99a51bbcbd80d500e9a8cb1ed891dd
Avner Eisenberg <avner.e@pepperi.com>
2021-10-05 18:18:11 +0300

    no message

--- sub-addons/slideshow/client-side/src/app/components/slideshow-editor/slideshow-editor.component.ts
+++ sub-addons/slideshow/client-side/src/app/components/slideshow-editor/slideshow-editor.component.ts
@@ -1,7 +1,9 @@
 import { CdkDragEnd, CdkDragStart } from '@angular/cdk/drag-drop';
-import { Component, ElementRef, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { PepStyleType, PepSizeType, PepHorizontalAlignment, PepVerticalAlignment} from '@pepperi-addons/ngx-lib';
+import { PepStyleType, PepSizeType, PepHorizontalAlignment, PepVerticalAlignment, PepScreenSizeType} from '@pepperi-addons/ngx-lib';
+import { IPepButtonClickEvent } from '@pepperi-addons/ngx-lib/button';
 // import { PageSizeType } from '@pepperi-addons/papi-sdk';
 
 // type UiPageSizeType = PageSizeType | 'NONE';
@@ -13,7 +15,7 @@ import { PepStyleType, PepSizeType, PepHorizontalAlignment, PepVerticalAlignment
 export type HeightUnit = 'REM' | 'VH';
 
 export type TransitionType = 'Fade' | 'Blur' | 'Dissolve' | 'Iris';
-export type ArrowType = '>>' | '>' | '-->';
+export type ArrowType = 'Two' | 'One' | 'Styled';
 export type ArrowShape = 'None' | 'Rect' | 'Rounded';
 export type WidthUnits = 'Narrow' | 'Regular' | 'Wide';
 export type Intensity = 'Soft' | 'Regular';
@@ -43,26 +45,28 @@ export type DropShadow = {
     intensity:  Intensity
 }
 
-export interface ISlideshowEditor {
-    id: string,
-    heightUnit?: HeightUnit,
-    height: string,
-    innerSpacing?: PepSizeType,
-    isTransition: boolean,
-    transitionDuration: number,
-    transitionType?: TransitionType,
-    transitionTime: string,
-    isUseArrows: boolean,
-    arrowType: ArrowType,
-    arrowShape: ArrowShape,
-    arrowStyle: PepStyleType,
-    arrowColor: PepStyleType,
-    isControllers: boolean,
-    usePauseButton: boolean,
-    showOnMobile: boolean,
-    inverStyle: boolean,
-    showControllersInSlider: boolean,
-    controllerSize: HeightUnit 
+
+//export interface ISlideshowEditor {
+export class ISlideshowEditor {
+    id: string;
+    heightUnit: HeightUnit = "REM";
+    height: string = '300';
+    innerSpacing?: PepSizeType = "md";
+    isTransition: boolean = false;
+    transitionDuration: number = 10;
+    transitionType: TransitionType = 'Fade';
+    transitionTime: string = '5';
+    isUseArrows: boolean = true;
+    arrowType: ArrowType = 'One';
+    arrowShape: ArrowShape = 'Rect';
+    arrowStyle: PepStyleType = 'regular';
+    arrowColor: PepStyleType = 'weak';
+    useControllers: boolean = true;
+    usePauseButton: boolean = true;
+    showOnMobile: boolean = true;
+    useInverStyle: boolean = true;
+    showControllersInSlider: boolean = true;
+    controllerSize: HeightUnit
 }
 
 export interface ISlideEditor {
@@ -83,7 +87,6 @@ export interface ISlideEditor {
     overlay: Overlay,
     image: SlideImage,
     dropShadow: DropShadow
-
 }
 
 interface groupButtonArray {
@@ -96,6 +99,11 @@ export interface slide {
     Title?: string,
 }
 
+export interface ISlideShow{
+    slideshowConfig: ISlideshowEditor,
+    // slides: Array<ISlideEditor>;
+}
+
 @Component({
     selector: 'slideshow-editor',
     templateUrl: './slideshow-editor.component.html',
@@ -104,13 +112,33 @@ export interface slide {
 export class SlideshowEditorComponent implements OnInit {
     
     @ViewChild('availableSlidesContainer', { read: ElementRef }) availableBlocksContainer: ElementRef;
-    @Input() slidesDropList = []; 
+
+    // @Input() slidesDropList = []; 
+    
+    //private hostObject: ISlideShow;
+    // @Input() 
+    // set hostObject(value: ISlideShow) {
+    //     this._hostObject = value;
+    // }
+    // get hostObject(): ISlideShow {
+    //     return this._hostObject;
+    // }
+
+    @Output() hostObjectChange: EventEmitter<ISlideShow> = new EventEmitter<ISlideShow>();
+    
+    hostObject: ISlideShow = { slideshowConfig: new ISlideshowEditor()};
+    slideShowConfig = new ISlideshowEditor();
+
     availableSlides: Array<slide> = [{id: '1', Title: "Slide1"},{id: '2', Title: "Slide2"}];
+    
+    transitionTypes: Array<{key: TransitionType, value: string}>;
+    transitionTimes: Array<{key: string, value: string}>;
+    arrowStyles: Array<{key: PepStyleType, value: string}>;
 
     HeightUnitsType: Array<groupButtonArray>;
-    InnerSpacing: Array<groupButtonArray>;
+    InnerSpacing: Array<{key: PepSizeType, value: string}>;
     ArrowsType: Array<groupButtonArray>;
-    ArrowButtons: Array<groupButtonArray>;
+    ArrowButtons: Array<{key: ArrowShape, value: string}>;
     ControllerSize: Array<groupButtonArray>;
     
     SlideTitleSize:Array<groupButtonArray>;
@@ -121,13 +149,8 @@ export class SlideshowEditorComponent implements OnInit {
     SlideDropShadowStyle: Array<groupButtonArray>;
 
     showSlideEditor = false;
-    isTransition = true;
-    useArrows = true;
 
-    slideshowHeight: '100';
-    
-   
-
+    // SLIDE ATTR
     showSlideTitle = false;
     slideContent = '';
     showSubTitle = false;
@@ -139,16 +162,123 @@ export class SlideshowEditorComponent implements OnInit {
     useDropShadow = false;
     
 
-    constructor(
-        private translate: TranslateService
-        // private utilitiesService: PepUtilitiesService
-    ) { 
+    constructor(private translate: TranslateService) { 
+        this.hostObject.slideshowConfig = new ISlideshowEditor();
+        debugger;
+    }
 
+    private updateHostObject() {
+        this.hostObject.slideshowConfig = this.slideShowConfig;
+        this.hostObjectChange.emit(this.hostObject);
+    }
+
+
+    onSlideshowFieldChange(key, event){
+        switch(key){
+            case 'heightUnits': {
+                this.slideShowConfig.heightUnit = event.source.key;
+                break;
+            }
+            case 'height':{
+                this.slideShowConfig.height = event;
+                break;
+            }
+            case 'innerspacing': {
+                this.slideShowConfig.innerSpacing = event.source.key;
+                break;
+            }
+            case 'duration':{
+                this.slideShowConfig.transitionDuration = event;
+                break;
+            }
+            case 'isTransition':{
+                
+                this.slideShowConfig.isTransition = event;
+                break;
+            } 
+            case 'transitionType':{
+                this.slideShowConfig.transitionType = event;
+                break;
+            } 
+            case 'transitionTime':{
+                this.slideShowConfig.transitionTime = event;
+                break;
+            } 
+            case 'useArrows':{
+                this.slideShowConfig.isUseArrows = event;
+                break;
+            } 
+            case 'arrowType':{
+                this.slideShowConfig.arrowType = event.source.key;
+                break;
+            } 
+            case 'arrowShape':{
+                this.slideShowConfig.arrowShape = event.source.key;
+                break;
+            } 
+            case 'arrowsStyle':{
+                this.slideShowConfig.arrowStyle = event;
+                break;
+            } 
+            case 'arrowsColor':{
+                this.slideShowConfig.arrowColor = event;
+                break;
+            } 
+            case 'useControllers':{
+                this.slideShowConfig.useControllers = event;
+                break;
+            } 
+            case 'usePauseBtn':{
+                this.slideShowConfig.usePauseButton = event;
+                break;
+            } 
+            case 'showOnMobile':{
+                this.slideShowConfig.showOnMobile = event;
+                break;
+            } 
+            case 'useInverStyle':{
+                this.slideShowConfig.useInverStyle = event;
+                break;
+            } 
+            case 'showControllersInSlider':{
+                this.slideShowConfig.showControllersInSlider = event;
+                break;
+            } 
+            
+        }
+        this.updateHostObject();
+    }
+
+    onIsTransitionChange(value: boolean) {
+        this.slideShowConfig.isTransition = value;
+        this.updateHostObject();
     }
 
     async ngOnInit(): Promise<void> {
-        const desktopTitle = await this.translate.get('PAGE_MANAGER.DESKTOP').toPromise();
 
+        const desktopTitle = await this.translate.get('SLIDESHOW.HEIGHTUNITS_REM').toPromise();
+
+        this.transitionTypes = [
+            { key: 'Fade', value: this.translate.instant('SLIDESHOW.TRANSITIONTYPES.FADE') },
+            { key: 'Blur', value: this.translate.instant('SLIDESHOW.TRANSITIONTYPES.BLUR') },
+            { key: 'Dissolve', value: this.translate.instant('SLIDESHOW.TRANSITIONTYPES.DISSOLVE') },
+            { key: 'Iris', value: this.translate.instant('SLIDESHOW.TRANSITIONTYPES.IRIS') }
+        ]
+
+        this.transitionTimes = [
+            { key: '3', value: '3'},
+            { key: '5', value: '5'},
+            { key: '7', value: '7'},
+            { key: '10', value: '10'},
+            { key: '15', value: '15'},
+        ]
+        
+        this.arrowStyles = [
+            { key: 'weak', value: 'Weak' },
+            { key: 'weak-invert', value:'Weak-Invert' },
+            { key: 'regular', value: 'Regular' },
+            { key: 'strong', value: 'Strong' }
+        ];
 
         this.HeightUnitsType = [
             { key: 'REM', value: this.translate.instant('SLIDESHOW.HEIGHTUNITS_REM') },
@@ -156,20 +286,20 @@ export class SlideshowEditorComponent implements OnInit {
         ];
     
         this.InnerSpacing = [
-            { key: 'SM', value: this.translate.instant('GROUP_SIZE.SM') },
-            { key: 'MD', value: this.translate.instant('GROUP_SIZE.MD') }
+            { key: 'sm', value: this.translate.instant('GROUP_SIZE.SM') },
+            { key: 'md', value: this.translate.instant('GROUP_SIZE.MD') }
         ];
     
         this.ArrowsType = [
-            { key: 'TWO', value: this.translate.instant('SLIDESHOW.ARROW_TYPE.TWO_ARROWS') },
-            { key: 'ONE', value: this.translate.instant('SLIDESHOW.ARROW_TYPE.ONE_ARROW') },
-            { key: 'STYLED', value: this.translate.instant('SLIDESHOW.ARROW_TYPE.STYLED_ARROW') },
+            { key: 'Two', value: this.translate.instant('SLIDESHOW.ARROW_TYPE.TWO_ARROWS') },
+            { key: 'One', value: this.translate.instant('SLIDESHOW.ARROW_TYPE.ONE_ARROW') },
+            { key: 'Styled', value: this.translate.instant('SLIDESHOW.ARROW_TYPE.STYLED_ARROW') },
         ];
     
         this.ArrowButtons = [
-            { key: 'NONE', value: this.translate.instant('GROUP_SIZE.NONE') },
-            { key: 'RECT', value: this.translate.instant('SLIDESHOW.ARROW_BUTTON.RECT') },
-            { key: 'ROUNDED', value: this.translate.instant('SLIDESHOW.ARROW_BUTTON.ROUNDED') }
+            { key: 'None', value: this.translate.instant('GROUP_SIZE.NONE') },
+            { key: 'Rect', value: this.translate.instant('SLIDESHOW.ARROW_BUTTON.RECT') },
+            { key: 'Rounded', value: this.translate.instant('SLIDESHOW.ARROW_BUTTON.ROUNDED') }
         ];
     
         this.ControllerSize = [
@@ -214,9 +344,7 @@ export class SlideshowEditorComponent implements OnInit {
 
     }
 
-    onSlideshowHeightChange(event){
-        
-    }
+  
 
     onAddContentClick(e) {
         //this.pageBuilderService.addSection();
@@ -233,7 +361,10 @@ export class SlideshowEditorComponent implements OnInit {
     onSlideEditClick(event){
         this.showSlideEditor = true;
          //this.pageBuilderService.navigateToEditor('section', this.id);
-        debugger;
+    }
+
+    navigateBack() {
+        this.showSlideEditor = false;
     }
 
     onValueChange(event){
